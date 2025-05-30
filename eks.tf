8c828e1825924a63bee4b84a34b66349bbe41ab4
Samuel Victor <samuel@samuelvictor.dev>
2025-05-30 10:54:35 -0300

    remove commented blocks

--- eks.tf
+++ eks.tf
@@ -26,24 +26,4 @@ resource "aws_eks_cluster" "eks-cluster" {
     endpoint_public_access = true
     endpoint_private_access = false
   }
-
-  # access_config {
-  #   authentication_mode = var.accessConfig
-  # }
 }
-
-# resource "aws_eks_access_entry" "eks-access-entry" {
-#   cluster_name  = aws_eks_cluster.eks-cluster.name
-#   principal_arn = var.principalArn
-#   type          = "STANDARD"
-# }
-
-# resource "aws_eks_access_policy_association" "eks-access-policy" {
-#   cluster_name  = aws_eks_cluster.eks-cluster.name
-#   policy_arn    = var.policyArn
-#   principal_arn = var.principalArn
-
-#   access_scope {
-#     type = "cluster"
-#   }
-# }
